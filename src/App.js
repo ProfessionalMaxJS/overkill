d0f38e706c6a6c13d27f95a3af9d259069c9665b
Max Engel-Streich <maxees@gmail.com>
2022-11-14 23:17:35 -0500

    elloGuvnah

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const elloGuvnah = () => {
+  console.log("elloGuvnah!!")
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" onClick={elloGuvnah} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
