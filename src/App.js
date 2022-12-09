928d8c78a90699ceaa6ce84953f896f3ce65c7b3
Max Engel-Streich <maxees@gmail.com>
2022-12-09 09:40:03 -0500

    new non-deprecated oauth

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const elloGuvnah = () => {
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" onClick={elloGuvnah} />
@@ -19,11 +20,27 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
         </a>
-      </header>
-    </div>
+        <script src="https://accounts.google.com/gsi/client" async defer></script>
+      <div id="g_id_onload"
+         data-client_id="YOUR_GOOGLE_CLIENT_ID"
+         data-login_uri="https://your.domain/your_login_endpoint"
+        //  data-auto_prompt="false"
+         >
+      </div>
+      <div class="g_id_signin"
+         data-type="standard"
+         data-size="large"
+         data-theme="outline"
+         data-text="sign_in_with"
+         data-shape="rectangular"
+         data-logo_alignment="left">
+      </div>
+          </header>
+        </div>
+</>
   );
 }
 
